05dc5e4378283ebc5f9dd1c2efeb3279d3be4136
Gabriel <gabrieloliveira.desenvolvedor03@gmail.com>
2024-05-15 15:02:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function abrirNovaAba(url) {
+    window.open(url, '_blank');
+}
+
+// AQUI FIZEMOS UMA FUNÇÃO PARA ABRIR UMA NOVA PAGINA NA WEB A CADA VEZ QUE EU CLICAR EM CADA "DIV" E REDICIONAR A CADA LINK.
